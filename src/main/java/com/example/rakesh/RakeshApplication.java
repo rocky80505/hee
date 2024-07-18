c1ebc9bfe985cf82a8bd736c2e20ae426ac77218
rakesh <rakeshrakee440@gmail.com>
2024-07-18 15:19:31 +0530

    hi

---
+++ src/main/java/com/example/rakesh/RakeshApplication.java
@@ -0,0 +1,13 @@
+package com.example.rakesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rakesh/RakeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rakesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
